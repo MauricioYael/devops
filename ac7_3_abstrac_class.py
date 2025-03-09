f3c99ae2e2a0ed22eee882a330ce5f7be10f5542
MauricioYael <pastenjardon@gmail.com>
2025-03-08 23:05:37 -0600

    Add files via upload

---
+++ ac7_3_abstrac_class.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class AbsNumeros(ABC):
+    
+    @abstractmethod
+    def print_numeros(self):
+        pass
+    
+    
+class Racionales(AbsNumeros):
+    
+    def __init__(self,n):
+        self.n = n
+        
+    def print_numeros(self):
+        print("Hola Mauricio, El numero racional es: ", self.n)
+        print("El numero racional en forma de fraccion es: ", self.n.as_integer_ratio())
---
+++ act7_2_modulo.py
@@ -0,0 +1,18 @@
+import act7_1_numeros
+import ac7_3_abstrac_class
+
+print("Hola Mauricio, Ingrese un numero: ")
+n = int(input())
+enteros = act7_1_numeros.Numeros(n)
+enteros.print_numeros()
+
+print("Ingresa un numero racional: ")
+q = float(input())
+racionales = act7_1_numeros.Racionales(q)
+racionales.print_numeros()
+
+print("Ingrese un numero para el modulo abstracto:")
+m = float(input())
+racionales = ac7_3_abstrac_class.Racionales(m)
+racionales.print_numeros()
+
