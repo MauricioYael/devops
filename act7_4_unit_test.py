c530b269f8e372596fe3363a63795988fcb5ea26
MauricioYael <pastenjardon@gmail.com>
2025-03-08 23:01:24 -0600

    Add files via upload

---
+++ act7_4_unit_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from act7_1_numeros import Racionales
+
+class TestRacionalesMethods(unittest.TestCase):
+    
+    def test_print_hello(self):
+        r = Racionales(2.5)
+        self.assertEqual(r.print_hello(), "Hola Mauricio, soy un número racional.")
+
+if __name__ == '__main__':
+    unittest.main()
